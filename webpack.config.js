a2225e4c81c95337a512f31c742d7cc8d86b3652
rene-buikman <rene.buikman@qonqord.com>
2026-04-17 16:33:45 +0200

    Configure for GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/SortAssets/',
   },
   entry: './src/index.ts',
   plugins: [
